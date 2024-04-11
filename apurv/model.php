bf12fd4b9783d2b46392226486b69e60d11a769a
apurv <prajaptiappurv@gmail.com>
2024-04-11 21:46:03 +0530

    indentation and file model

---
+++ apurv/model.php
@@ -0,0 +1,35 @@
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Encodevilla</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputname4">Practical Name</label>
+                            <input type="practicalName" class="form-control" id="inputname4" placeholder="Practical Name">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Details</label>
+                            <input type="text" class="form-control" id="inputPassword4" placeholder="Enter Your Details">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputAddress">practical File Name</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="Index.html">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlFile1">choice img</label>
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
