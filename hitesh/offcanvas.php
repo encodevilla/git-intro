7ee7659a9bdc85035c936f0788bef221a8d74535
hitesh <hittu9173@gmail.com>
2024-04-11 15:09:24 -1100

    offcanvas create

---
+++ hitesh/offcanvas.php
@@ -0,0 +1,53 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<nav class="navbar fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Encodevilla</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
+                </button>
+            </div>
+            <form class="d-flex mt-3" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php" target="_blank">Home</a>
+                    </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Apurv
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#" target="_blank">coming soon</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Hitesh
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="Hitesh/myintrodaction_pr1.html" target="_blank">      practical 1</a></li>
+                            <li><a class="dropdown-item" href="hitesh/mycontact_pr2.html" target="_blank">      practical 2</a></li>
+                            <li><a class="dropdown-item" href="hitesh/mydetails_pr3.html" target="_blank">      practical 3</a></li>
+                            <li><a class="dropdown-item" href="hitesh/my_examnation_pepar_p4.html" 
+                                  target="_blank">practical 4</a></li>
+                            <li><a class="dropdown-item" href="hitesh/my_fav_sub_pr5.html" target="_blank">      practical 5</a></li>
+                            <li><a class="dropdown-item" href="hitesh/my_fav_drink_pr6.html" target="_blank">      practical 6</a></li>
+                            <li><a class="dropdown-item" href="hitesh/navicationbar_pr7.html" target="_blank">      practical 7</a></li>
+                            <li><a class="dropdown-item" href="hitesh/navicationbar_with_alldata_pr8.html"      target="_blank">practical 8</a></li>
+                            <li><a class="dropdown-item" href="hitesh/homepage.html" target="_blank">           practical 9</a></li>
+                            <li><a class="dropdown-item" href="hitesh/id_login_page.html" target="_blank">      practical 10</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
